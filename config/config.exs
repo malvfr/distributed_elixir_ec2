e1b61e3536bb8bb2a58187b28be2daebdd79d684
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-08-07 19:47:06 -0300

    Remove sensitive info

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,8 @@
 import Config
 
 config :ex_aws,
-  access_key_id: [{:system, "AKIAUCS2EDSLHSE56JHT"}, :instance_role],
-  secret_access_key: [{:system, "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"}, :instance_role]
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
 
 config :ex_aws,
   region: "us-east-1"
