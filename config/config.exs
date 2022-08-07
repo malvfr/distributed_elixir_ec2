813ca8fda21112af3cc245f7b8bbaa5fc5cb1a84
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-08-07 19:39:59 -0300

    config file

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :ex_aws,
+  access_key_id: [{:system, "AKIAUCS2EDSLHSE56JHT"}, :instance_role],
+  secret_access_key: [{:system, "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"}, :instance_role]
+
+config :ex_aws,
+  region: "us-east-1"
