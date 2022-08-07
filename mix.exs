af453594646540283a05f629fb04ccf2b08bfe67
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-08-07 16:58:57 -0300

    Update version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DistributedEc2.MixProject do
     [
       app: :distributed_ec2,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
