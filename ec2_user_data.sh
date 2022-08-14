73726f8adaefc0ae09fd9202628ee32bd159448f
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-08-14 17:58:47 -0300

    Update link

--- ec2_user_data.sh
+++ ec2_user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
 sudo apt-get update -y
-wget https://filebin.net/6ppfivmjzfzoz2v7/output.tar.gz
+wget https://filebin.net/nrln04ij4f8i0jfb/output.tar.gz
 tar -zxvf output.tar.gz
 _build/dev/rel/distributed_ec2/bin/distributed_ec2 daemon_iex
